057d2ec259dc9ff3b65e456ba94d5d6a8a43eb0f
Sandesh003 <sandesh.fluxbyte@gmail.com>
2023-01-04 10:38:29 +0530

    made a paginated api

---
+++ index.js
@@ -0,0 +1,61 @@
+import express from "express";
+
+const app = express();
+
+let data = [
+  {
+    id: 1,
+    city: 'ahmedabad'
+  },
+  {
+    id: 2,
+    city: 'gandhinagar'
+  },
+  {
+    id: 3,
+    city: 'mehsana'
+  },
+  {
+    id: 4,
+    city: 'rajkot'
+  },
+  {
+    id: 5,
+    city: 'bharuch'
+  },
+  {
+    id: 6,
+    city: 'surat'
+  },
+  {
+    id: 7,
+    city: 'vadodara'
+  },
+  {
+    id: 8,
+    city: 'navsari'
+  },
+  {
+    id: 9,
+    city: 'valsad'
+  },
+  {
+    id: 10,
+    city: 'vapi'
+  }
+]
+
+app.get('/',(req, res)=>{
+  let page = req.query.page ?? 1
+  let limit = req.query.limit ?? data.length
+
+  let startPoint = (page - 1) * limit;
+  let endPoint = page * limit;
+
+  const result = data.slice(startPoint,endPoint)
+  res.json(result);
+})
+
+app.listen(3000,()=>{
+  console.log("app is running...")
+})
